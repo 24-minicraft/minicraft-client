34fc37fe86e29d4958107cffd90631d58bbe5fba
EnsDevelop <chemins60217@gmail.com>
2024-04-12 13:48:58 +0900

    feat: add password input

---
+++ src/components/auth/passwordInput/index.tsx
@@ -0,0 +1,29 @@
+import { VisibleIcon } from "@/assets/icons"
+import Input, { IInputProps } from "../common/input"
+import "./style.scss"
+import { LabelInput } from "../common/labelInput"
+
+interface IPasswordInput extends Pick<IInputProps, "value" | "onChange" | "name"> {
+    label: string
+    visible: boolean
+    onToggle: () => void
+}
+
+export const PasswordInput = ({ label, value, onChange, visible, onToggle, name = "password" }: IPasswordInput) => {
+    return (
+        <LabelInput label={label}>
+            <div className="passwordInputBlockOutSide">
+                <div className="passwordInputBlockInSide">
+                    <Input
+                        type={visible ? "text" : "password"}
+                        value={value}
+                        onChange={onChange}
+                        name={name}
+                        disabled={false}
+                    />
+                    <VisibleIcon visible={visible} onClick={onToggle} />
+                </div>
+            </div>
+        </LabelInput>
+    )
+}
