2ace4f9551fbd8e5fa705929d2d3a0a97c6c5adf
EnsDevelop <chemins60217@gmail.com>
2024-04-28 10:55:00 +0900

    design: Modify background css

--- src/libs/layout/startLayout/index.tsx
+++ src/libs/layout/startLayout/index.tsx
@@ -3,7 +3,7 @@ import { background } from "@/assets/images"
 
 export const StartLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="container" style={{ backgroundImage: `url(${background})` }}>
+        <div className="container" style={{ background: `url(${background}) no-repeat center/cover` }}>
             <div className="wrapper">{children}</div>
         </div>
     )
