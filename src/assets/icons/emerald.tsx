03d1469e161fb91ed68394b3dd5080ab9c3103b3
EnsDevelop <chemins60217@gmail.com>
2024-05-20 09:57:28 +0900

    feat :: add emerald icon

---
+++ src/assets/icons/emerald.tsx
@@ -0,0 +1,20 @@
+const Emerald = () => {
+    return (
+        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="32" height="32" fill="url(#pattern0_150_26280)" />
+            <defs>
+                <pattern id="pattern0_150_26280" patternContentUnits="objectBoundingBox" width="1" height="1">
+                    <use xlinkHref="#image0_150_26280" transform="scale(0.0625)" />
+                </pattern>
+                <image
+                    id="image0_150_26280"
+                    width="16"
+                    height="16"
+                    xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQBAMAAADt3eJSAAAAIVBMVEUAAADb/+uv/c2C9q1B84QX3WIAqiwAlSkAexgAUwAALQC5+lfcAAAAAXRSTlMAQObYZgAAAENJREFUeNpjwApmzoTQnCKuE8CMiSpuUWABIRW31AUghmqQWhmUYdaGxgg2a4UyPEpBDIZpFe1WEAPTgTJgsGoVVpsBDRwS6h42ddsAAAAASUVORK5CYII="
+                />
+            </defs>
+        </svg>
+    )
+}
+
+export default Emerald
