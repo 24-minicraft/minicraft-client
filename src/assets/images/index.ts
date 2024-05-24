38fd4257448712c77b172322ed4394899136732a
EnsDevelop <chemins60217@gmail.com>
2024-05-24 09:56:04 +0900

    docs :: move bg image

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,2 +1,4 @@
-import background from "./background.png"
-export { background }
+export { default as background } from "./background.png"
+export { default as bg } from "./bg.png"
+export { default as GrassBlockImg } from "./grassBlock.png" // 잔디블록
+export { default as StoneBlockImg } from "./stoneBlock.png" // 돌블록
