c4c9a6944fd4eecb6bf3d03520c937dbaad81d7c
EnsDevelop <chemins60217@gmail.com>
2024-05-24 09:57:19 +0900

    feat :: apply game layout

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,16 +1,49 @@
 import { NowArea } from "@/components/area/nowArea"
 import { CharacterList } from "@/components/character/characterList"
+import Inventory from "@/components/inventory"
+import ResourceContainer from "@/containers/resource"
+import SeedsContainer from "@/containers/seeds"
 import { GameLayout } from "@/libs/layout/gameLayout"
 
 const GamePage = () => {
     return (
         <GameLayout
-            seedNode={<div>seed 수집 컴포넌트</div>}
+            seedNode={
+                <>
+                    <SeedsContainer />
+                    <Inventory
+                        column={4}
+                        row={8}
+                        data={[
+                            {
+                                type: "LOG",
+                                amount: 8,
+                            },
+                            {
+                                type: "STICK",
+                                amount: 12,
+                            },
+                            {
+                                type: "STRAW",
+                                amount: 22,
+                            },
+                            {
+                                type: "COAL",
+                                amount: 24,
+                            },
+                            {
+                                type: "COBBLESTONE",
+                                amount: 64,
+                            },
+                        ]}
+                    />
+                </>
+            }
             characterListNode={<CharacterList />}
             regionNode={<NowArea select />}
             settingNode={<div>설정</div>}
         >
-            <p>자원 수집</p>
+            <ResourceContainer />
         </GameLayout>
     )
 }
