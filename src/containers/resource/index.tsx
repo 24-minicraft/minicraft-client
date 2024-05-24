7ebefd865125be1effa2eaef1bd9789cc9d71d81
EnsDevelop <chemins60217@gmail.com>
2024-05-24 09:59:01 +0900

    feat :: add item collect component

--- src/containers/resource/index.tsx
+++ src/containers/resource/index.tsx
@@ -1,11 +1,36 @@
-import { LogItem, StrawItem } from "@/assets/items"
+import { useCollectItem } from "@/apis/item"
+import { ItemType } from "@/apis/item/type"
 import Resource from "@/components/resource"
+import { itemComponent } from "@/libs/constant/itemComponent"
+import { useState } from "react"
 
 const ResourceContainer = () => {
+    const { mutate } = useCollectItem()
+    const [loadingStates, setLoadingStates] = useState({
+        LOG: true,
+        STRAW: true,
+    })
+
+    const handleClick = (type: ItemType, time: number) => {
+        setLoadingStates((prevState) => ({ ...prevState, [type]: false }))
+        setTimeout(() => {
+            mutate(type)
+            setLoadingStates((prevState) => ({ ...prevState, [type]: true }))
+        }, time * 1000)
+    }
+
     return (
         <div style={{ display: "flex", gap: "300px", height: "100%", justifyContent: "center", alignItems: "center" }}>
-            <Resource item={<LogItem size={128} />} />
-            <Resource item={<StrawItem size={128} />} />
+            <Resource
+                item={itemComponent["LOG"][128]}
+                onclick={() => handleClick("LOG", 4)}
+                loading={loadingStates["LOG"]}
+            />
+            <Resource
+                item={itemComponent["STRAW"][128]}
+                onclick={() => handleClick("STRAW", 10)}
+                loading={loadingStates["STRAW"]}
+            />
         </div>
     )
 }
